fd9c5a6bd992b9e7822dba90ca83a6f734982242
Noah <noahglennmaldonado@gmail.com>
2025-03-18 08:54:30 -0400

    completed basic styling of hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h1>Portfolio Website</h1>
+    <main className="flex min-h-screen flex-col bg-[#121212] ">
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
