61d4f9e93e2f73227b414916df45021bafcd166f
Noah <noahglennmaldonado@gmail.com>
2025-03-28 07:05:32 -0400

    hero section and navbar complete and fully responsive

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+
+const AboutSection = () => {
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image
+          alt="self-photo"
+          src={"/images/about-image.png"}
+          width={500}
+          height={500}
+        />
+        <div>
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+            pariatur hic ea incidunt reiciendis quidem a labore ullam sequi at.
+            Perspiciatis atque non dolore temporibus facere consequuntur debitis
+            accusamus laborum!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
